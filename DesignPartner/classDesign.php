e6f6b3ae6f0ed38615b21b8656b5d8ad218b0468
筒井智也 <tomoya.tmy@gmail.com>
2019-09-15 19:24:39 +0900

    feat: Book機能 追加・出力

--- DesignPartner/classDesign.php
+++ DesignPartner/classDesign.php
@@ -1,5 +1,30 @@
 <?php
 
 class Book {
-    p
+
+    private $title;
+    private $pages;
+    private $author;
+
+    function setBookDetails($title, $pages, $author)
+    {
+        $this->title = $title;
+        $this->pages = $pages;
+        $this->author = $author;
+    }
+
+    function getBookDetails()
+    {
+        $bookDetail = array($this->title, $this->pages, $this->author);
+
+        return
+            $bookDetail[0]. "\n".
+            $bookDetail[1]."\n".
+            $bookDetail[2]."\n";
+    }
+
 }
+
+$book = new Book();
+$book->setBookDetails('君の名は', '200ページ', '新海誠');
+echo $book->getBookDetails();
