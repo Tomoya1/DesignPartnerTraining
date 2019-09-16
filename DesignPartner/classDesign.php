b7cce4a0d7d6b4e4b1c67eba37f963832ad20d8e
筒井智也 <tomoya.tmy@gmail.com>
2019-09-16 12:06:32 +0900

    add: ３冊出力する処理の実装

--- DesignPartner/classDesign.php
+++ DesignPartner/classDesign.php
@@ -1,30 +1,40 @@
 <?php
 
 class Book {
-
+    private $books;
     private $title;
     private $pages;
     private $author;
 
     function setBookDetails($title, $pages, $author)
     {
-        $this->title = $title;
-        $this->pages = $pages;
-        $this->author = $author;
+        $book = array($title, $pages, $author);
+        $this->books[] = $book;
+        $this->title[] = $title;
+        $this->pages[] = $pages;
+        $this->author[] = $author;
     }
 
     function getBookDetails()
     {
-        $bookDetail = array($this->title, $this->pages, $this->author);
-
-        return
-            $bookDetail[0]. "\n".
-            $bookDetail[1]."\n".
-            $bookDetail[2]."\n";
+        return $this->books;
     }
 
 }
 
 $book = new Book();
 $book->setBookDetails('君の名は', '200ページ', '新海誠');
-echo $book->getBookDetails();
+
+$book2 = new Book();
+$book->setBookDetails('オブジェクト指向でなぜつくるのか', '365ページ', '平澤章');
+
+$book2 = new Book();
+$book->setBookDetails('Head First デザインパターン', '568ページ', 'オライリージャパン');
+
+$bookDetails = $book->getBookDetails();
+
+foreach ($bookDetails as $book){
+    echo $book[0]."\n";
+    echo $book[1]."\n";
+    echo $book[2]."\n";
+}
