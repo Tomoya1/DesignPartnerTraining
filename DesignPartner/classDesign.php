621a6bdc463ca352479cedcc5a7339db179cb307
筒井智也 <tomoya.tmy@gmail.com>
2019-09-16 20:29:13 +0900

    issue#4実装

--- DesignPartner/classDesign.php
+++ DesignPartner/classDesign.php
@@ -27,16 +27,37 @@ class Book {
 
 }
 
+function searchBooks($title, $books)
+{
+    $titleList = array_column($books, 'title');
+    $expectTitleNum = array_search($title, $titleList);
+    return $books[$expectTitleNum];
+}
+
+$books = [];
+
 $book = new Book();
 $book->setTitle('吾輩は猫である');
 $book->setPages(100);
+$books[] = ['title' => $book->getTitle(), 'pages' => $book->getPages()];
 
-echo $book->getTitle(); //「我輩は猫である」と表示される
-echo $book->getPages(); //「100」と表示される
 
 $book2 = new Book();
 $book2->setTitle('坊ちゃん');
 $book2->setPages(200);
+$books[] = ['title' => $book2->getTitle(), 'pages' => $book2->getPages()];
 
-echo $book2->getTitle(); //「坊ちゃん」と表示される
-echo $book2->getPages(); //「200」と表示される
+$book3 = new Book();
+$book3->setTitle('それから');
+$book3->setPages(300);
+$books[] = ['title' => $book3->getTitle(), 'pages' => $book3->getPages()];
+
+foreach ($books as $book)
+{
+    echo $book['title']."\n";
+}
+
+// $booksの中から、 タイトルが「坊ちゃん」であるものを取り出しましょう。 多次元配列から取得する
+$bookDetails = searchBooks('坊ちゃん', $books);
+echo $bookDetails['title'];
+echo $bookDetails['pages'];
